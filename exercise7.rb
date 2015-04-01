ddbe6dd8110a6657ffe6dd21c602b9d68c216c37
Dan Comand <dcomand@gmail.com>
2015-04-01 16:49:49 -0400

    Deleted second cohort, finished assignment.

--- exercise7.rb
+++ exercise7.rb
@@ -20,19 +20,19 @@ cohorts(students)
 puts students.keys
 
 def new_totals(new_students)
-
 	new_students.each do |key, value|
 		new_value = (value*1.05).to_i
 		puts new_value
     new_students[key] = new_value
 	end
-
 end
 
 
-
+new_totals(students)
 cohorts(students)
 
+students.delete(:cohort2)
+cohorts(students)
 
 
 
