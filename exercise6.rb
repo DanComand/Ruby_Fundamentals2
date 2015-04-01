5e94c8aeda4bc1e782e24c5b1f4b5e16e0b18c32
Dan Comand <dcomand@gmail.com>
2015-04-01 15:09:39 -0400

    Created method to re-use list code, and added rice to list.

--- exercise6.rb
+++ exercise6.rb
@@ -1,5 +1,14 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+list = ["carrots", "toilet paper", "apples", "salmon"]
+
+
+def groceries(grocery_list)
 grocery_list.each do |item|
 	puts "* #{item}"
 
 end
+	end
+
+list.push("rice")
+groceries(list)
+
+
